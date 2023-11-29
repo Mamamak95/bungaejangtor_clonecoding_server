4fef009322871579a1a784de77d635907a7c095c
Ho <little95ho@gmail.com>
2023-11-29 10:24:53 +0900

    update server.js from ho
    linked to sign,login router
    
    2023-11-29

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 import express from "express";
 import cors from "cors";
-import chatRouter from './router/chatRouter.js'  
-import productRouter from './router/productRouter.js'  
+import chatRouter from "./router/chatRouter.js";
+import productRouter from "./router/productRouter.js";
 
 const server = express();
-server
-  .use(cors())
-  .use(express.json());
-  
-server.use('/chat',chatRouter)
-server.use('/product',productRouter)
-server.use('/productImg',express.static('productImg'))
-server.use('/webImg',express.static('webImg'))
+const PORT = 8000;
+server.use(cors())
+      .use(express.json())
+      .use(express.urlencoded());
 
-server.listen(8000,()=>{
-  console.log('server start')
-})
+server.use("/sign", signRouter);
+server.use("/login", loginRouter);
+server.use("/chat", chatRouter);
+server.use("/product", productRouter);
+server.use("/productImg", express.static("productImg"));
+server.use("/webImg", express.static("webImg"));
+
+server.listen(PORT, () => {
+  console.log(`server running --> ${PORT}`);
+});
