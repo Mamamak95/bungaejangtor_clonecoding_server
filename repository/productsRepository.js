62c2f291d73f65c1d627f14730fcc80887191605
음정태 <dmawjdxo12@naver.com>
2023-12-01 16:38:31 +0900

    상숨 상세 from Eum
    상품상세 데이터 서버 연동
    
    2023-12-01

--- repository/productsRepository.js
+++ repository/productsRepository.js
@@ -49,3 +49,76 @@ export async function productImg(pid,img) {
     .execute(sql,[pid,img])
     .then((result) => 'success')
 }
+
+export async function detail(pid) {
+  const sql = `
+  SELECT 
+  p.pid,
+  p.seller,
+  p.regdate,
+  p.content,
+  p.productName,
+  FORMAT(p.price, 0) AS price,
+  GROUP_CONCAT(pi.img) AS images,
+  u.img AS userImage,
+  (SELECT COUNT(pid) FROM product WHERE seller = p.seller) AS total_pid_count
+FROM 
+  product p
+JOIN 
+  productImage pi ON p.pid = pi.pid
+LEFT JOIN
+  user u ON p.seller = u.uid
+WHERE 
+  p.pid = ?
+GROUP BY
+  p.pid, p.seller, p.regdate, p.content, p.productName, p.price, u.img
+                                
+                `
+  return db
+    .execute(sql,[pid])
+    .then((result) => result[0])
+}
+
+export async function similiar() {
+  const sql = `
+              SELECT 
+              DISTINCT p.pid, 
+              p.productName,
+              first_value(pi.img) over (PARTITION BY p.pid ORDER BY pi.pid) AS img,
+              left(p.regdate, 4) regdate,
+              format(p.price,0) price, 
+              first_value(pi.img) over(order by pi.pid)
+            FROM 
+              product p JOIN productImage pi 
+            ON 
+              p.pid = pi.pid
+            LIMIT 10;
+                          
+                `
+  return db
+    .execute(sql,[])
+    .then((result) => result[0])
+}
+
+export async function shop(seller) {
+  const sql = `
+                  SELECT 
+                  DISTINCT p.pid, 
+                  p.productName,
+                  p.seller,
+                  first_value(pi.img) over (PARTITION BY p.pid ORDER BY pi.pid) AS img,
+                  left(p.regdate, 4) regdate,
+                  format(p.price,0) price, 
+                  first_value(pi.img) over(order by pi.pid)
+                FROM 
+                  product p JOIN productImage pi 
+                ON 
+                  p.pid = pi.pid
+                  and seller = ?
+                LIMIT 6;
+                          
+                `
+  return db
+    .execute(sql,[seller])
+    .then((result) => result[0])
+}
