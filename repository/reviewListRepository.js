03384febbf656a4491ea5f782e080a940fd9ecbf
음정태 <dmawjdxo12@naver.com>
2023-12-22 16:53:48 +0900

    2023-12-22 발표

--- repository/reviewListRepository.js
+++ repository/reviewListRepository.js
@@ -8,7 +8,7 @@ export async function getReview(id) {
   SELECT review.rid, review.uid, review.score, review.content, user.img ,review.date
   FROM review
   JOIN user ON review.target = user.uid
-  WHERE review.target = ? AND user.uid = ?;
+  WHERE review.target = ? AND user.uid = ? order by review.date desc;
                 `
   return db
     .execute(sql, [id,id])
